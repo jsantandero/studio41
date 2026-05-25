06b720fed14fb4fa5af07e38dedcf6037cf671ab
Jorge <jorge.santander.o@gmail.com>
2026-05-25 15:14:41 -0400

    save generated images

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -1,12 +1,25 @@
 import { NextResponse } from "next/server";
+
 import OpenAI from "openai";
+
+import { createClient } from "@supabase/supabase-js";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
+);
+
 export async function POST(req: Request) {
+
   try {
+
+    const body = await req.json();
+
+    console.log(body.image);
 
     const result = await openai.images.generate({
       model: "gpt-image-1",
@@ -30,19 +43,44 @@ do not redesign the product
 
     const imageBase64 = result.data?.[0]?.b64_json;
 
+    if (!imageBase64) {
+      throw new Error("No image generated");
+    }
+
+    const buffer = Buffer.from(
+      imageBase64,
+      "base64"
+    );
+
+    const fileName = `generated-${Date.now()}.png`;
+
+    const { error } = await supabase.storage
+      .from("generated-images")
+      .upload(fileName, buffer, {
+        contentType: "image/png",
+      });
+
+    if (error) {
+      throw error;
+    }
+
+    const { data } = supabase.storage
+      .from("generated-images")
+      .getPublicUrl(fileName);
+
     return NextResponse.json({
       success: true,
       image: imageBase64,
+      imageUrl: data.publicUrl,
     });
 
   } catch (error: any) {
 
-    console.error("OPENAI ERROR:");
     console.error(error);
 
     return NextResponse.json({
       success: false,
-      error: error?.message || "generation failed",
+      error: error.message,
     });
   }
 }
